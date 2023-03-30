5882ad75747e76c48e78a10637ec0f72c23b8254
Pkanand1999 <singhisking810267@gmail.com>
2023-03-30 18:49:56 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import { Navbar } from "./Routes/Navbar";
-import { AllRoutes } from "./Routes/AllRoutes";
 export default function App() {
   return (
     <div className="App">
